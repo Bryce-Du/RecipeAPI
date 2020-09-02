afc6a37e915b58071b28aa6fe8be52172f254fad
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-09-02 13:56:22 -0400

    jwt setup
    this might be over my head, I might try to do simpler user auth without jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     has_many :ingredients, through: :users_ingredients
     has_many :users_recipes
     has_many :recipes, through: :users_recipes
+
+    has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }
 end
