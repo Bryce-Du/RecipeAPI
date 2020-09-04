757b8a86d26c4c64d83240ab21606923df81c838
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-09-04 13:34:44 -0400

    set up nested user_recipe path

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,14 @@
 class RecipesController < ApplicationController
+    before_action :get_user
+    
     def index
-        @recipes = Recipe.all
+        @recipes = (@user ? @user.recipes : Recipe.all)
         render json: RecipeSerializer.new(@recipes)
+    end
+
+    private
+
+    def get_user
+        @user = User.find(params[:user_id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   post '/login', to: 'session#create'
   resources :recipes
   resources :ingredients
+  resources :users do
+    resources :recipes, only: [:index]
+    resources :ingredients, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
