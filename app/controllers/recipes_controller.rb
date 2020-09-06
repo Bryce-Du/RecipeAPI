b22ffeacb0fc8117be8150f79cdcc3f36d0f8eb2
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-09-05 20:45:53 -0400

    make create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,31 @@
 class RecipesController < ApplicationController
-    before_action :get_user
+    before_action :get_user 
     
     def index
         @recipes = (@user ? @user.recipes : Recipe.all)
         render json: RecipeSerializer.new(@recipes)
     end
 
+    def create
+        @recipe = Recipe.new(recipe_params)
+        if @recipe.save
+            params[:ingredients].each do |ingr|
+                ingredient = Ingredient.find_by(name: ingr[:name])
+                RecipesIngredient.create(recipe_id: @recipe.id, ingredient_id: ingredient.id, quantity: ingr[:quantity])
+            end
+            UsersRecipe.create(user_id: @user.id, recipe_id: @recipe.id)
+            render json: RecipeSerializer.new(@recipe)
+        else
+            render json: { error: 'failed to create recipe' }, status: :not_acceptable
+        end
+        byebug
+    end
+
     private
 
+    def recipe_params
+        params.require(:recipe).permit(:name, :instructions)
+    end
     def get_user
         @user = User.find(params[:user_id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :recipes
   resources :ingredients
   resources :users do
-    resources :recipes, only: [:index]
+    resources :recipes, only: [:index, :create]
     resources :ingredients, only: [:index]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
